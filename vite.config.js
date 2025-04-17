59626d5cbe92aee9a2584ed418d1106258f50ea0
amirhamza05 <amir.hamza@streamstech.com>
2025-04-17 15:56:08 +0600

    Update build

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,23 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
-
+import htmlMinifier from 'vite-plugin-html-minifier';
 // https://vite.dev/config/
 var url = "https://websote.tserm.com";
 
 
 export default defineConfig({
-  plugins: [vue(), tailwindcss()],
+  plugins: [
+    vue(), 
+    tailwindcss(),
+    htmlMinifier({
+      collapseWhitespace: true, // Remove spaces and tabs
+      removeComments: true,     // Remove HTML comments
+      removeAttributeQuotes: true, // Optional: Remove quotes from attributes when safe
+      minifyJS: true,            // Minify inline JavaScript
+      minifyCSS: true            // Minify inline CSS
+    })
+  ],
   css: {
     preprocessorOptions: {
       tailwind: {
